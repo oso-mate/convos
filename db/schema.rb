217174d2a43a5a80bfe15947c95abcd23effda34
Luis Flores <oso.mate@gmail.com>
2014-11-06 19:24:39 -0800

    Unique usernames

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107013123) do
+ActiveRecord::Schema.define(version: 20141107032100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,7 @@ ActiveRecord::Schema.define(version: 20141107013123) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
 
 end
